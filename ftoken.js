59517c7eba057753329e31d90d3502b0c7675662
aifakepro <aifake.pro@gmail.com>
2025-10-22 21:53:31 +0300

    Update ftoken.js

--- ftoken.js
+++ ftoken.js
@@ -1,11 +1,12 @@
-const resp = await fetch("/api/token", { method: "POST" });
+// Получаем токен с вашего сервера
+const resp = await fetch("https://liveki-tserver.vercel.app/api/token?room=StopAIFake", { method: "POST" });
 const data = await resp.json();
 const token = data.token;
 
-const room = "test-room";
 const url = "wss://stopaifake-07gwd6vp.livekit.cloud";
 
 const roomConnection = await LiveKit.connect(url, token, { 
   audio: true,
   video: false 
 });
+
